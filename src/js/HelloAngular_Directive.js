7b290af2e65dd98981294ec574b819a037f30bdb
qianyj <qianyj@cairenhui.com>
2015-07-21 17:30:58 +0800

    h5game

--- src/js/HelloAngular_Directive.js
+++ src/js/HelloAngular_Directive.js
@@ -15,5 +15,20 @@ function HelloAngular($scope){
 	$scope.test=function(){
 		alert('test')
 	}
+	$scope.person={
+		firstName:'lala',
+		lastName:'fAAa',
+		year:'2223123',
+		type:[
+		{name:'dad',country:'asdfasdf'},
+		{name:'dad',country:'asdfasdf'},
+		{name:'dad',country:'asdfasdf'}
+		]
+		fullName:function(){
+			var x;
+			x=$scope.person;
+			return x.firstName+x.lastName
+		}
+	}
 }
 
---
+++ src/js/demo.js
@@ -0,0 +1,9 @@
+require.config({
+	paths:{
+		jquery:'jquery-2.1.4.min'
+	}
+})
+
+require(['jquery'],function($){
+	alert(1)
+})
